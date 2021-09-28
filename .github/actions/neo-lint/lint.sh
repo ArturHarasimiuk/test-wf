6acf79cd3c118796de8545fb3ff6ef8b04448bbb
Artur Harasimiuk <artur.harasimiuk@intel.com>
2021-09-28 23:31:19 +0200

    update script
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- .github/actions/neo-lint/lint.sh
+++ .github/actions/neo-lint/lint.sh
@@ -5,6 +5,14 @@
 # SPDX-License-Identifier: MIT
 #
 
+set -x
+
 pwd
+ls -la
+git -C neo remote -vv
+git -C neo log
+git -C neo log origin/master
+git -C neo show origin/master
+git -C neo show HEAD
 git -C neo diff -U0 --no-color --relative origin/master..HEAD | clang-format-diff-11 -p1 -i
 git -C neo status
