808b4839cdb6804aff6bf71f65e3581aaabab40a
Artur Harasimiuk <artur.harasimiuk@intel.com>
2021-09-29 00:49:59 +0200

    update
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- .github/actions/neo-lint/lint.sh
+++ .github/actions/neo-lint/lint.sh
@@ -7,8 +7,12 @@
 
 set -ex
 
+clang-format-11 --version
+
 git -C neo fetch origin ${GITHUB_BASE_REF}
 git -C neo diff -U0 --no-color --relative origin/${GITHUB_BASE_REF}..HEAD | clang-format-diff-11 -p1 -i
+git -C neo status
+
 if [ -n "$(git -C neo status --porcelain)" ]; then
     git -C neo status
     exit 1
