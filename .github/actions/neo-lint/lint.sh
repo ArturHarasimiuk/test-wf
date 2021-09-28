cc57121f92b846ff009be4b8b79446cc5626fbf8
Artur Harasimiuk <artur.harasimiuk@intel.com>
2021-09-29 00:46:22 +0200

    fix
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- .github/actions/neo-lint/lint.sh
+++ .github/actions/neo-lint/lint.sh
@@ -5,11 +5,11 @@
 # SPDX-License-Identifier: MIT
 #
 
-set -e
+set -ex
 
 git -C neo fetch origin ${GITHUB_BASE_REF}
 git -C neo diff -U0 --no-color --relative origin/${GITHUB_BASE_REF}..HEAD | clang-format-diff-11 -p1 -i
-if [ -n "\$(git -C neo status --porcelain)" ]; then
+if [ -n "$(git -C neo status --porcelain)" ]; then
     git -C neo status
     exit 1
 fi
