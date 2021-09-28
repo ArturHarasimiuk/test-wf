4aff06becf54475e2d2b2b832acc1a9154c9818f
Artur Harasimiuk <artur.harasimiuk@intel.com>
2021-09-29 01:03:32 +0200

    fix linter
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- .github/actions/neo-lint/lint.sh
+++ .github/actions/neo-lint/lint.sh
@@ -10,7 +10,10 @@ set -ex
 clang-format-11 --version
 
 git -C neo fetch origin ${GITHUB_BASE_REF}
-git -C neo diff -U0 --no-color --relative origin/${GITHUB_BASE_REF}..HEAD | clang-format-diff-11 -p1 -i
+(
+    cd neo
+    git diff -U0 --no-color origin/master..HEAD | clang-format-diff-11 -p1 -i
+)
 git -C neo status
 
 if [ -n "$(git -C neo status --porcelain)" ]; then
