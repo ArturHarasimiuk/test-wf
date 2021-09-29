fd5aedf0e5f5b51682fc329fda2075615a2aa415
Artur Harasimiuk <artur.harasimiuk@intel.com>
2021-09-29 10:28:19 +0200

    use INPUT_PATH in action
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- .github/actions/neo-lint/lint.sh
+++ .github/actions/neo-lint/lint.sh
@@ -7,17 +7,17 @@
 
 set -e
 
-env
-
 clang-format-11 --version
 
+INPUT_PATH="${INPUT_PATH:-.}"
+
 (
-    cd neo
+    cd ${INPUT_PATH}
     git fetch origin ${GITHUB_BASE_REF}
     git diff -U0 --no-color origin/master..HEAD | clang-format-diff-11 -p1 -i -v -iregex '.*\.(cpp|h|inl)'
 )
 
-if [ -n "$(git -C neo status --porcelain)" ]; then
-    git -C neo diff
+if [ -n "$(git -C ${INPUT_PATH} status --porcelain)" ]; then
+    git -C ${INPUT_PATH} diff
     exit 1
 fi
