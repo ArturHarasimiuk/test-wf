b8967500b0ff51436984348f9b15b36f8cd8d395
Artur Harasimiuk <artur.harasimiuk@intel.com>
2021-09-29 11:09:32 +0200

    fix iregex setup
    Signed-off-by: Artur Harasimiuk <artur.harasimiuk@intel.com>

--- .github/actions/neo-lint/lint.sh
+++ .github/actions/neo-lint/lint.sh
@@ -10,13 +10,13 @@ set -e
 clang-format-11 --version
 
 INPUT_PATH="${INPUT_PATH:-.}"
-INPUT_IREGEX="${INPUT_IREGEX:-'.*\.(cpp|h|inl)'}"
+INPUT_IREGEX="${INPUT_IREGEX:-.*\.(cpp|h|inl)}"
 
 (
     cd ${INPUT_PATH}
     git fetch origin ${GITHUB_BASE_REF}
     set -x
-    git diff -U0 --no-color origin/master..HEAD | clang-format-diff-11 -p1 -i -v -iregex${INPUT_IREGEX}
+    git diff -U0 --no-color origin/master..HEAD | clang-format-diff-11 -p1 -i -v -iregex ${INPUT_IREGEX}
     set +x
 )
 
